69ab0bed12781a0c8079c181d967b5d95786cd7a
fusbudget <andrewsanser@gmail.com>
2022-02-03 16:46:48 +0100

    add Card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Card from "./components/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <div>
+        <Header />
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    );
   }
 }
 
