c393a3a58f691c2a16bbec30c5ea7e341c87e477
Charles Dubois <charles.dubois.h@gmail.com>
2022-02-07 12:20:46 +0100

    click on my card

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,22 +4,36 @@ import "./Main.module.css";
 import Card from "./Card";
 
 class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardProps: [
+        { id: 1, path: "/images/img1.jpg" },
+        { id: 2, path: "/images/img2.jpg" },
+        { id: 3, path: "/images/img3.jpg" },
+        { id: 4, path: "/images/img4.jpg" },
+        { id: 5, path: "/images/img5.jpg" },
+        { id: 6, path: "/images/img6.jpg" },
+      ],
+    };
+  }
+
   render() {
     return (
       <main>
         <section className="container-fluid">
           <div className="row d-flex justify-content-around">
             <div className="card col-2">
-              <Card></Card>
+              <Card cardProps={this.state.cardProps[0]}></Card>
             </div>
             <div className="card col-2">
-              <Card></Card>{" "}
+              <Card cardProps={this.state.cardProps[0]}></Card>{" "}
             </div>
             <div className="card col-2">
-              <Card></Card>{" "}
+              <Card cardProps={this.state.cardProps[1]}></Card>{" "}
             </div>
             <div className="card col-2">
-              <Card></Card>{" "}
+              <Card cardProps={this.state.cardProps[1]}></Card>{" "}
             </div>
           </div>
 
@@ -53,13 +67,6 @@ class Main extends React.Component {
             </div>
           </div>
         </section>
-
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
       </main>
     );
   }
